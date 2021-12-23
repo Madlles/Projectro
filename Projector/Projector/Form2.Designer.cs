6bb07305ccf75e3277f10b5414aff26e6ffe5360
Madlles <mf288062@gmail.com>
2021-12-23 20:33:22 +0300

    -Добавлено полноценный вход/регистрация

---
+++ Projector/Projector/Form2.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Projector
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.действияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.местоНахождениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.конфигурацияПкToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.главноеМенюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.PC_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Location = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Configyration_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.действияToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(602, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // действияToolStripMenuItem
+            // 
+            this.действияToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.местоНахождениеToolStripMenuItem,
+            this.конфигурацияПкToolStripMenuItem,
+            this.главноеМенюToolStripMenuItem});
+            this.действияToolStripMenuItem.Name = "действияToolStripMenuItem";
+            this.действияToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.действияToolStripMenuItem.Text = "Действия";
+            // 
+            // местоНахождениеToolStripMenuItem
+            // 
+            this.местоНахождениеToolStripMenuItem.Name = "местоНахождениеToolStripMenuItem";
+            this.местоНахождениеToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.местоНахождениеToolStripMenuItem.Text = "Место Нахождение";
+            this.местоНахождениеToolStripMenuItem.Click += new System.EventHandler(this.местоНахождениеToolStripMenuItem_Click);
+            // 
+            // конфигурацияПкToolStripMenuItem
+            // 
+            this.конфигурацияПкToolStripMenuItem.Name = "конфигурацияПкToolStripMenuItem";
+            this.конфигурацияПкToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.конфигурацияПкToolStripMenuItem.Text = "Конфигурация Пк";
+            this.конфигурацияПкToolStripMenuItem.Click += new System.EventHandler(this.конфигурацияПкToolStripMenuItem_Click);
+            // 
+            // главноеМенюToolStripMenuItem
+            // 
+            this.главноеМенюToolStripMenuItem.Name = "главноеМенюToolStripMenuItem";
+            this.главноеМенюToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.главноеМенюToolStripMenuItem.Text = "Главное меню";
+            this.главноеМенюToolStripMenuItem.Click += new System.EventHandler(this.главноеМенюToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem1});
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // выходToolStripMenuItem1
+            // 
+            this.выходToolStripMenuItem1.Name = "выходToolStripMenuItem1";
+            this.выходToolStripMenuItem1.Size = new System.Drawing.Size(109, 22);
+            this.выходToolStripMenuItem1.Text = "Выход";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Menu;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.PC_ID,
+            this.Location,
+            this.Configyration_ID});
+            this.dataGridView1.Location = new System.Drawing.Point(86, 25);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(430, 105);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // PC_ID
+            // 
+            this.PC_ID.HeaderText = "Номер ПК";
+            this.PC_ID.MinimumWidth = 6;
+            this.PC_ID.Name = "PC_ID";
+            this.PC_ID.Width = 125;
+            // 
+            // Location
+            // 
+            this.Location.HeaderText = "Место нахождения";
+            this.Location.MinimumWidth = 6;
+            this.Location.Name = "Location";
+            this.Location.Width = 125;
+            // 
+            // Configyration_ID
+            // 
+            this.Configyration_ID.HeaderText = "Номер конфигурации";
+            this.Configyration_ID.MinimumWidth = 6;
+            this.Configyration_ID.Name = "Configyration_ID";
+            this.Configyration_ID.Width = 125;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 279);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(274, 249);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(209, 233);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите номер ПК, который хотите найти";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Projector.Properties.Resources._121013_red_background_hd_2048x1152_for_retina;
+            this.ClientSize = new System.Drawing.Size(602, 372);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximumSize = new System.Drawing.Size(618, 411);
+            this.MinimumSize = new System.Drawing.Size(618, 411);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Главное меню сотрудника";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem действияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem местоНахождениеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem конфигурацияПкToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem главноеМенюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PC_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Location;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Configyration_ID;
+    }
+}
---
+++ Projector/Projector/Form2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Projector
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void местоНахождениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form3 frm3 = new Form3();
+            frm3.Owner = this; //Передаём вновь созданной форме её владельца.
+            frm3.Show();
+            Hide();
+        }
+
+        private void конфигурацияПкToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form4 frm4 = new Form4();
+            frm4.Owner = this;
+            frm4.Show();
+            Hide();
+        }
+
+        private void главноеМенюToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2();
+            frm2.Owner = this;
+            frm2.Show();
+            Hide();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=SystemDataBase.mdb";
+            OleDbConnection dbConnection = new OleDbConnection(connectionString);
+
+            dbConnection.Open();
+            string query = "SELECT * FROM DB";
+            OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);
+            OleDbDataReader dbReader = dbCommand.ExecuteReader();
+
+            if (dbReader.HasRows == false)
+            {
+                MessageBox.Show("Ошибка");
+            }
+            else
+            {
+                while (dbReader.Read())
+                {
+                    dataGridView1.Rows.Add(dbReader["PC_ID"], dbReader["Location"], dbReader["ID_Configyration"]);
+                }
+            }
+
+            dbReader.Close();
+            dbConnection.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
